0aa3ce79d69fb957d485180c488e734fa07f15a1
setarekhosravi <strhkhosravi@gmail.com>
2024-07-22 09:17:08 +0330

    create first node

--- ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
+++ ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("first_node")
+        self.get_logger().info("Hello from ROS2")
 
 def main(args= None):
     rclpy.init(args=args)
+    node = MyNode()
 
-    # everthing you want should be written here
-    
     rclpy.shutdown()
 
 if __name__ == '__main__':
