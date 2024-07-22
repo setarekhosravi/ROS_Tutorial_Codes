180fa9e740d5c97971ab203ceb79c0accc80bc24
setarekhosravi <strhkhosravi@gmail.com>
2024-07-22 09:30:26 +0330

    keep nodes alive with spin

--- ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
+++ ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -12,7 +12,7 @@ class MyNode(Node):
 def main(args= None):
     rclpy.init(args=args)
     node = MyNode()
-
+    rclpy.spin(node) # spin node means keep node alive until you press CTRL+C and kill it
     rclpy.shutdown()
 
 if __name__ == '__main__':
