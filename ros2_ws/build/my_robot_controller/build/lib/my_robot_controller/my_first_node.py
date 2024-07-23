e20f37fcd78c398fb9d42123bcd3122383610b22
setarekhosravi <strhkhosravi@gmail.com>
2024-07-23 10:16:55 +0330

    add timer callback

--- ros2_ws/build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
+++ ros2_ws/build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
@@ -7,7 +7,10 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("first_node")
-        self.get_logger().info("Hello from ROS2")
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
 
 def main(args= None):
     rclpy.init(args=args)
