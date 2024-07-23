38bdffdf8d3212082b4289c4994221192d2f9a84
setarekhosravi <strhkhosravi@gmail.com>
2024-07-23 10:22:01 +0330

    add counter

--- ros2_ws/build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
+++ ros2_ws/build/my_robot_controller/build/lib/my_robot_controller/my_first_node.py
@@ -7,15 +7,17 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("first_node")
+        self.counter = 0
         self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
-        self.get_logger().info("Hello")
+        self.get_logger().info("Hello" + str(self.counter))
+        self.counter += 1
 
 def main(args= None):
     rclpy.init(args=args)
     node = MyNode()
-    rclpy.spin(node) # spin node means keep node alive until you press CTRL+C and kill it
+    rclpy.spin(node) # spin node means keep node alive until you press CTRL+C and kill it, enable all callbacks
     rclpy.shutdown()
 
 if __name__ == '__main__':
