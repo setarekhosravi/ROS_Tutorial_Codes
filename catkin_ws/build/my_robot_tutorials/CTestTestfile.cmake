c4bb91ab0953d9a8bc7cfaf07d7c3bd83db324bb
setarekhosravi <strhkhosravi@gmail.com>
2025-02-24 20:57:39 +0330

    delete files

--- catkin_ws/build/my_robot_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/strh/Robotics/ROS_Tutorial_Codes/catkin_ws/src/my_robot_tutorials
-# Build directory: /home/strh/Robotics/ROS_Tutorial_Codes/catkin_ws/build/my_robot_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
